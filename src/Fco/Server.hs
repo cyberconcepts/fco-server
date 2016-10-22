462fa77456c373ad0573b0d3d1d77adfcad862c3
Helmut Merz <helmutm@cy55.de>
2016-10-22 20:28:51 +0200

    small steps forward...

--- src/Fco/Server.hs
+++ src/Fco/Server.hs
@@ -1,25 +1,31 @@
 {-# LANGUAGE DataKinds, TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
 
 module Fco.Server (
     startApp
 ) where
 
+import BasicPrelude
 import Data.Aeson
 import GHC.Generics
 import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
+
 type API = Get '[JSON] [Command]
+  :<|> "commands" :> Get '[JSON] [Command]
+
 
 data Command = Command {
-  name :: String,
-  description :: String,
-  url :: String
+  name :: Text,
+  description :: Text,
+  url :: Text
 } deriving (Eq, Show, Generic)
 
 instance ToJSON Command
+
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -30,10 +36,24 @@ app = serve api server
 api :: Proxy API
 api = Proxy
 
+
 server :: Server API
-server = return commands
+server = return commandsRoot
+    :<|> return commands
+
+
+commandsRoot :: [Command]
+commandsRoot =
+  [Command "commands" "Get a list of available commands" 
+           "http://localhost:8080/commands"
+  ]
 
 commands :: [Command]
 commands =
-  [Command "commands" "Get a list of available commands" "http://localhost:8080/commands"
+  [Command "commands" "Get a list of available commands" 
+           "http://localhost:8080/commands",
+   Command "namespaces" "Get a list of available namespaces" 
+           "http://localhost:8080/namespaces",
+   Command "triples" "Get info on stored triples/quads" 
+           "http://localhost:8080/triples"
   ]
